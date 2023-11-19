d6e4285ea44f8a6965bf03d0714a7d4a59d120eb
Piero <piero.cahuana@ucsm.edu.pe>
2023-11-18 23:09:44 -0500

    Codigo de polimorfisco a implementar con la clase principal Cuenta

---
+++ MenusPolimorfismo/AplicacionPresupuesto.java
@@ -0,0 +1,8 @@
+package MenusPolimorfismo;
+
+public class AplicacionPresupuesto  {
+    //se implementa las funcionalidades del planificador
+    //Se implementa lista de usuarios 
+    //public void establecerPresupuestoMensual ()
+    //public void generarInforme() {
+}
---
+++ MenusPolimorfismo/PlanificadorPresupuesto.java
@@ -0,0 +1,6 @@
+package MenusPolimorfismo;
+
+public interface PlanificadorPresupuesto {
+    //Generar informe de gastos y presupuesto usado
+    
+}
