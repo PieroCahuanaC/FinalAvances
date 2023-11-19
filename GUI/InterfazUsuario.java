ca1444e3af31ae6eb1bcdf30ae132faecbe633f0
Piero <piero.cahuana@ucsm.edu.pe>
2023-11-18 23:11:55 -0500

    ga

--- GUI/InterfazUsuario.java
+++ GUI/InterfazUsuario.java
@@ -3,4 +3,7 @@ package GUI;
 public class InterfazUsuario {
     //mostrar tipo un menu de banco
     //interfaz de bcp
+    //Se mostrara todas las funcionalidades de las clases anteriores en una interfaz amigable para la interaccion
+    //Se mostrara como el codigo final
+    //Implementara o heredara las clases anteriores
 }
